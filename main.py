61cf081eb62dc1b2a7e0d1de167a1f233652cc1a
xiaoruange39 <2234884717@qq.com>
2026-07-11 14:13:40 +0800

    chore: release version 2.1.8

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def _install_feature_methods(*feature_classes):
     return decorator
 
 
-@register("astrbot_plugin_message_stats", "xiaoruange39", "群发言统计插件", "2.1.7")
+@register("astrbot_plugin_message_stats", "xiaoruange39", "群发言统计插件", "2.1.8")
 @_install_feature_methods(WebPanelMixin, StatsMixin, RankingMixin)
 class MessageStatsPlugin(Star):
     """群发言统计插件
